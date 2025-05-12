801c78346b24815c1444c339db37f2bfd11acdac
Cristhian <cristhian.bravo.8657@gmail.com>
2025-05-11 19:00:14 -0500

    add ejercicio_resuelto_1: Programa en python, para probar la interfaz virtual, en el que se identifica si una matriz es escalar

---
+++ ejercicio_resuelto_1.py
@@ -0,0 +1,26 @@
+def esEscalar(m):
+    d = m[0][0]
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if i != j:
+                if m[i][j] != 0:
+                    return False
+            elif m[i][j] != d:
+                return False
+    return True
+
+#Matriz escalar
+matriz1 = [
+    [3, 0, 0],
+    [0, 3, 0],
+    [0, 0, 3]
+]
+#Matriz no escalar
+matriz2 = [
+    [2, 0, 1],
+    [0, 2, 0],
+    [0, 0, 2]
+]
+
+print(esEscalar(matriz1))  # Verdadero
+print(esEscalar(matriz2))  # Falso
